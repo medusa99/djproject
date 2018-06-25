8f223c6c6f1eda8b644bf8156d7e8a9ee5c94f0d
medusa99 <tjamesonedm@gmail.com>
2018-06-25 14:41:32 -0400

    template display

---
+++ djangoproject/posts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+#we want to load an index method in the views file. we want a route for just /post (not posts/something)
+# ^$ start with and end with nothing. second param looks into the views file for an index method, and then give it a name. this is
+#the first route.
+urlpatterns = [
+      url ( r'^$', views.index, name='index')
+        ];
