2ac8ff2cdd27a27785e85a10b47d50a3c5cfcedc
medusa99 <tjamesonedm@gmail.com>
2018-06-25 16:41:25 -0400

    'Display template'

--- djangoproject/posts/views.py
+++ djangoproject/posts/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+# Create your views here..
 def index(request):
 #    return HttpResponse('HELLO FROM POSTS')
      return render(request, 'posts/index.html')
