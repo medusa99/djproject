156082677ec49bac3a5e98f07b03cb2bd1197ee4
medusa99 <tjamesonedm@gmail.com>
2018-06-26 11:10:23 -0400

    Hardcode title in view as presented in layout

--- djangoproject/posts/views.py
+++ djangoproject/posts/views.py
@@ -6,4 +6,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 #    return HttpResponse('HELLO FROM POSTS')
-     return render(request, 'posts/index.html')
+     return render(request, 'posts/index.html', {
+         'title': 'Latest Posts'
+})
