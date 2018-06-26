5f39591fef1b241696e02917a0418ac58c6f1693
medusa99 <tjamesonedm@gmail.com>
2018-06-26 16:58:18 -0400

    iterate over posts to display on root posts page

--- djangoproject/posts/views.py
+++ djangoproject/posts/views.py
@@ -3,9 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 # Create your views here.
 def index(request):
 #    return HttpResponse('HELLO FROM POSTS')
-     return render(request, 'posts/index.html', {
-         'title': 'Latest Posts'
-})
+#so now our view should be getting posts that come from the model
+#go to html index page and loop the posts
+    posts = Posts.objects.all()[:10]
+    context = {
+      'title': 'Latest Posts',
+      'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
+ #got rid of the dictionary and pased in var context.
